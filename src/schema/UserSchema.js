60ecdb010b9c0dd5df671793a4d6d1bf442d49c2
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-26 23:24:46 +0530

    udpate user schema

--- src/schema/UserSchema.js
+++ src/schema/UserSchema.js
@@ -7,13 +7,14 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Please provide a name'],
       trim: true,
-      unique: true,
       minlength: [3, 'Name must be at least 3 characters'],
+      unique: true,
     },
     email: {
       type: String,
       required: [true, 'Please provide an email'],
       trim: true,
+      lowercase: true,
       unique: true,
       match: [
         /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
@@ -26,23 +27,15 @@ const userSchema = new mongoose.Schema(
       minlength: [4, 'Password must be at least 4 characters'],
       select: false,
     },
-    role: {
-      type: String,
-      enum: ['Owner', 'Collaborator', 'Viewer'],
-      default: 'Viewer',
-    },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-userSchema.pre('save', function modifyPassword(next) {
+userSchema.pre('save', function () {
   const user = this;
   const SALT = bcrypt.genSaltSync(9);
   const hashPassword = bcrypt.hashSync(user.password, SALT);
   user.password = hashPassword;
-  next();
 });
 
 const User = mongoose.model('User', userSchema);
