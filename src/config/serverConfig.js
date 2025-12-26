78542c25262553361f572cfafafaf654c6db1d21
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-26 18:00:04 +0530

    added server config

---
+++ src/config/serverConfig.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const PORT = process.env.PORT || 3000;
+export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const MONGODB_URI = process.env.DEV_DB_URL;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRY || '1d';
