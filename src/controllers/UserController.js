c856483d679c66833f6098dc3b4119ccf561b463
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-26 17:57:57 +0530

    added user controller layer

---
+++ src/controllers/UserController.js
@@ -0,0 +1,39 @@
+import { StatusCodes } from 'http-status-codes';
+import {
+  loginUserService,
+  registerUserService,
+} from '../services/UserService.js';
+
+export const registerUserController = async (req, res) => {
+  try {
+    const response = await registerUserService(req.body);
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      message: 'User successfully registered',
+      data: response,
+    });
+  } catch (error) {
+    console.error('Register User Controller Error: ', error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: 'Internal Server Error',
+    });
+  }
+};
+
+export const loginUserController = async (req, res) => {
+  try {
+    const response = await loginUserService();
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      message: 'User successfully registered',
+      data: response,
+    });
+  } catch (error) {
+    console.error('Login User Controller Error: ', error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: 'Internal Server Error',
+    });
+  }
+};
