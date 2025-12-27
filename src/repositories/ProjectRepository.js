5bee0b74fccd211a07a46a313f3005fd5326b19b
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-27 14:46:39 +0530

    add project repository

---
+++ src/repositories/ProjectRepository.js
@@ -0,0 +1,11 @@
+import { crudRepository } from './crudRepository.js';
+import Project from '../schema/ProjectSchema.js';
+
+const ProjectRepository = {
+  ...crudRepository(Project),
+  getProjectWithWorkspaceDetails: async function (projectId) {
+    return await Project.findById(projectId).populate('workspaceId');
+  }
+};
+
+export default ProjectRepository;
