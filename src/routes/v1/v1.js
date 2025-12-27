06e67987a97bc701c3e7ebda5eb3c8973e7d5633
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-27 18:59:57 +0530

    add workspace route

--- src/routes/v1/v1.js
+++ src/routes/v1/v1.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import userRoutes from './userRoutes.js';
+import workspaceRoutes from './workspaceRoutes.js';
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.get('/health', (req, res) => {
 });
 
 router.use('/users', userRoutes);
+router.use('/workspaces', workspaceRoutes);
 
 export default router;
