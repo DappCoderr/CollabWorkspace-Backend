e90cc943256d7a2184226a29d60e3af6a59c8c36
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-28 23:46:30 +0530

    added new paths

--- src/routes/v1/workspaceRoutes.js
+++ src/routes/v1/workspaceRoutes.js
@@ -1,17 +1,36 @@
 import express from 'express';
 import {
   addMemberToWorkspaceController,
+  addProjectToWorkspaceController,
   createWorkspaceController,
+  deleteWorkspaceController,
+  getAllWorkspacesController,
+  getWorkspaceController,
+  updateWorkspaceController,
 } from '../../controllers/WorkspaceController.js';
 import { isAuthenticated } from '../../middleware/isAuthenticated.js';
+import { getProjectsByWorkspaceIdController } from '../../controllers/ProjectController.js';
 
 const route = express.Router();
 
+// Public routes
+route.get('/:workspaceId', isAuthenticated, getWorkspaceController);
+route.get('/', isAuthenticated, getAllWorkspacesController);
+route.get('/:workspaceId', isAuthenticated, getProjectsByWorkspaceIdController);
+
+// Protected routes (requires `x-access-token` header)
 route.post('/', isAuthenticated, createWorkspaceController);
 route.post(
   '/:workspaceId/members',
   isAuthenticated,
   addMemberToWorkspaceController
 );
+route.post(
+  '/:workspaceId/projects',
+  isAuthenticated,
+  addProjectToWorkspaceController
+);
+route.put('/:workspaceId', isAuthenticated, updateWorkspaceController);
+route.delete('/:workspaceId', isAuthenticated, deleteWorkspaceController);
 
 export default route;
